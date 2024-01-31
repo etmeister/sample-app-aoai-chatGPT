312e20c937e7892f9b7a5be8aed25b5c9a5522f3
Eli Meister <eli.meister@ufl.edu>
2024-01-31 16:39:28 -0500

    button

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -7,15 +7,18 @@ interface ShareButtonProps extends IButtonProps {
 export const ShareButton: React.FC<ShareButtonProps> = ({onClick}) => {
     const shareButtonStyles: ICommandBarStyles & IButtonStyles = {
         root: {
-            width: '180px',
-            border: `1px solid #D1D1D1`,
-          },
-          rootHovered: {
-            border: `1px solid #D1D1D1`,
-          },
-          rootPressed: {
-            border: `1px solid #D1D1D1`,
-          },
+          width: 86,
+          height: 32,
+          borderRadius: 4,
+          background: 'linear-gradient(135deg, #0F6CBD 0%, #2D87C3 51.04%, #8DDDD8 100%)',
+        //   position: 'absolute',
+        //   right: 20,
+          padding: '5px 12px',
+          marginRight: '20px'
+        },
+        icon: {
+          color: '#FFFFFF',
+        },
         label: {
           fontWeight: 600,
           fontSize: 14,
